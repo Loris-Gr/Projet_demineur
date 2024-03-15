c2766e04a22d07ffba40e0a57e255e5a70c3bf87
Loris Grandchamp <loris.grandchamp@etu.univ-orleans.fr>
2024-03-15 16:48:31 +0100

    finition squelette plateau

--- src/Plateau.java
+++ src/Plateau.java
@@ -7,11 +7,20 @@ public class Plateau{
     private int nbColonnes ;
     private int pourcentageDeBombes ;
     private int nbBombes ;
+    private List<CaseIntelligente> lePlateau;
 
     public Plateau(int nbLignes, int nbColonnes, int pourcentage) {
         this.nbLignes = nbLignes;
         this.nbColonnes = nbColonnes;
         this.pourcentageDeBombes = pourcentage;
+    }
+
+    private void creerLesCasesVides() {
+
+    }
+
+    private void rendLesIntelligentes() {
+        
     }
 
     public int getNbLignes() {
