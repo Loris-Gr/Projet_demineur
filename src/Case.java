83e869e1a08f5c996d1b5d1d9fc7c5100ef38469
Loris Grandchamp <loris.grandchamp@etu.univ-orleans.fr>
2024-03-22 14:23:35 +0100

    complétion class case

--- src/Case.java
+++ src/Case.java
@@ -4,41 +4,44 @@ public class Case {
     private boolean estMarquee;
 
     public Case() {
+        this.reset();
+    }
+
+    public void reset() {
         this.contientUneBombe = false;
         this.estDecouverte = false;
         this.estMarquee = false;
     }
 
-    public void reset() {
-
+    public void poseBombe() {
+        this.contientUneBombe = true;
     }
 
-    public void poseBomb() {
-
-    }
-
-    public boolean ContientUneBombe() {
+    public boolean contientUneBombe() {
         return this.contientUneBombe;
     }
 
-    public boolean EstDecouverte() {
+    public boolean estDecouverte() {
         return this.estDecouverte;
     }
 
-    public boolean EstMarquee() {
+    public boolean estMarquee() {
         return this.estMarquee;
     }
 
     public boolean reveler() {
         if(!this.estDecouverte) {
             this.estDecouverte = true;
+            this.estMarquee = false;
             return true;
         }
         return false;
     }
 
     public void marquer() {
-        
+        if (!this.estDecouverte()) {
+            this.estMarquee = true;
+        }
     }
 }
 
